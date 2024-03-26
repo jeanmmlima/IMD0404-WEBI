3c5d7f15c240c22ec0a5308542e895e3c23b44c4
jeanmmlima <jeanmariomlima@gmail.com>
2024-03-26 15:55:52 -0300

    ajustes sobre manipulação do dom e introdução ao javascript

--- 08-javascript-intro-I/js/script5.js
+++ 08-javascript-intro-I/js/script5.js
@@ -1,93 +1,87 @@
-
-// Arrays
-
-var arrayHomogenea = ['segunda', 'terça', 'quarta'];
-var arrayHeterogeneo = ['domingo', 3, true];
-
-//Acessando elementos de arrays 
-
-const cidades = ['Natal', 'Fortaleza', 'Recife'];
-
-var minhaCidade = cidades[0];
-
-console.log(minhaCidade)
-console.log(cidades[2]);
-
-
-let cursosTecnicos = ['web', 'eletrônica', 'redes'];
-cursosTecnicos[1] = 'jogos';
-console.log(cursosTecnicos)
-
-// LENGTGH
-
-const listaDeCompras = ['ovos', 'leite', 'manteiga'];
-console.log(listaDeCompras.length);
-
-/*
-Os métodos push() e pop() são os métodos utilizados para realizar as tarefas 
-mais simples de manipulação de arrays, para adicionar e remover elementos.
-*/
-
-//PUSH
-const objetivos = ['estudar javascript', 'passar na disciplina'];
-objetivos.push('trabalhar com TI', 'relaxar');
-console.log(objetivos);
-
-//POP
-const objetivos2 = ['estudar javascript', 'passar na disciplina', 'trabalhar com TI'];
-var itemRemovido = objetivos2.pop();
-console.log(objetivos2); 
-console.log(itemRemovido); 
-
-//SHIFT - remove o primeiro elemento de um array. 
-
-const objetivos3 = ['estudar javascript', 'passar na disciplina', 'trabalhar com TI', 'relaxar'];
-var itemRemovido = objetivos3.shift();
-console.log(objetivos3);
-
-//UNSHIFT - Já o método unshift() permite adicionar itens ao início de um array. 
-
-const objetivos4 = ['estudar javascript', 'passar na disciplina', 'trabalhar com TI', 'relaxar'];
-objetivos4.unshift('a', 'b');
-console.log(objetivos4)
-
-/*
-Além dos métodos para manipular arrays do fim e do início dela, 
-temos métodos que permitem realizar ações a partir do meio de um array.
-
-splice() e slice()
-*/
-
-//splice() - O método splice() nos permite adicionar ou remover elementos no meio, ou em qualquer posição do array. Esse método modifica o array original.
-
-const mensagens = ['Mensagem 1', 'Mensagem 2', 'Mensagem 3'];
-
-mensagens.splice(1, 1);
-console.log(mensagens); 
-
-mensagens.splice(1, 0, 'Nova Mensagem');
-console.log(mensagens); 
-
-mensagens.splice(2, 1, 'Nova Mensagem 3');
-console.log(mensagens); 
-
 /* 
-O método slice() retorna um array contendo a cópia de uma parte de um array original, 
-selecionado de um índice inicial até um final (final não incluso). 
-Uma observação importante é que o array original não é modificado.
-*/ 
+Iterando Arrays em Javascript
+*/
 
-const letras = ['a', 'b', 'c', 'd', 'e', 'f'];
-let resultado = letras.slice(1, 4);
-console.log(resultado); // [ 'b', 'c', 'd' ]
-console.log(letras); // [ 'b', 'c', 'd' ]
+//### 1. forEach
+/**
+ * Esse é um método em que se executa uma função a cada 
+ * iteração de um array. Portanto o método forEach() recebe como argumento uma função, 
+ * também chamada de função de callback, com dois parâmetros: o valor e o índice do elemento.
+ */
 
-//concat()
+const array = ['João', 'Maria', 'Daniel', 'Alice'];
 
-const array1 = ['a', 'b'];
-const array2 = ['c', 'd'];
-const array3 = ['e', 'f'];
+array.forEach((elemento, indice) => { array[indice] = elemento.toUpperCase(); });
 
-const resultado_concat = array1.concat(array2).concat(array3);
+console.log(array);  // [ 'JOÃO', 'MARIA', 'DANIEL', 'ALICE' ]
 
-console.log(resultado_concat); // [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+//### 2. Métodos Every e Some
+/**
+ * O método every() retorna true ou false, baseado na condição estabelecida e em 
+ * todos os elementos do array em questão. É uma maneira interessante de 
+ * rapidamente checar se todos os elementos dentro de um array satisfazem um 
+ * determinado critério. 
+ */
+
+let numeros = [12, 19, 7, 33, 91, 77, 3, 11];
+
+let todosMaioresQueTrinta = numeros.every(n => {return n > 30});
+console.log('Todos os números são maiores que 30? ' + todosMaioresQueTrinta); // false
+
+/**
+ * Já o método some() também retorna true ou false, porém para retornar true 
+ * é suficiente que ao menos um dos elementos do array satisfaça o 
+ * critério estabelecido pela função passada como argumento. 
+ */
+
+let algumMaiorQueTrinta = numeros.some(n => {return n > 30});
+console.log('Tem algum número maior que 30? ' + algumMaiorQueTrinta); // true
+
+//### 3. filter, map e reduce !!!!!!!
+
+/** filter
+ * O filter() é um método que permite a criação de um novo array baseado nos 
+ * elementos de um array preexistente que satisfizerem o critério estabelecido
+ *  na função passada como argumento da função filter().
+ */
+
+const palavras = ['universidade', 'web', 'javascript', 'disciplina', 'aluno', 'nota'];
+const resultado = palavras.filter(palavra => palavra.length > 6);
+console.log(resultado); // [ 'universidade', 'javascript', 'disciplina' ]
+
+/** map
+ * O método map() cria uma nova array composta dos resultados da execução da 
+ * função passada como argumento em cada um dos elementos do array original. 
+ */
+
+const array2 = [1, 4, 9, 16];
+const resultado2 = array2.map(x => x * 2);
+console.log(resultado2); // [ 2, 8, 18, 32 ]
+
+/** reducer
+ * Por fim, o método reduce() executa uma função reducer (provida por você) para 
+ * cada membro do array, resultando num único valor de retorno.Para simplificar,
+ *  uma função reducer é uma função agregadora. Pode ser para fazer uma soma, 
+ * encontrar o maior ou menor valor numa lista, entre muitas outras possibilidades. 
+ * O reducer sempre percorrerá toda a lista e retornará apenas um resultado.
+ */
+
+const array3 = [1, 4, 9, 16];
+const resultado3 = array3.reduce( (acumulado, valor) => acumulado + valor );
+console.log(resultado3); // 30
+
+/**
+ * EXEMPLO FINAL
+ * Queremos filtrar somente os preços acima de R$15,00, 
+ * aplicar um desconto de 10% em cada um dos itens do array precos e 
+ * retornar o maior preço dos elementos reajustados.
+ */
+
+const precos = [7.50, 32.00, 12.50, 27.50, 16.50, 9.90];
+
+const maiorPrecoReajustado = precos
+                            .filter((produto) => { return produto > 15.00 })
+                            .map((produto) => { return produto - 0.1*produto })
+                            .reduce((a, b) => { return Math.max(a, b) } );                            
+
+console.log(maiorPrecoReajustado); // 28.8
