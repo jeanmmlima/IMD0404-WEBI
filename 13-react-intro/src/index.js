515f5df441d03af12777ce06a080b2ec2c66915e
jeanmmlima <jeanmariomlima@gmail.com>
2024-05-13 16:53:03 -0300

    timer ok

--- 13-react-intro/src/index.js
+++ 13-react-intro/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hello from './Hello';
+import Timer from './Timer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <div>
-      <Hello name="Paulo" />
-      <Hello name="Maria" />
-    </div>
-  </React.StrictMode>
-);
+
+
+
+function tick() {
+  root.render(
+    <Timer date={new Date()}/>
+  );
+}
+
+setInterval(tick, 1000);
 
 
 
