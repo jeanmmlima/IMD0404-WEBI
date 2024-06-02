c8fe06ebc1585373ae5c3ed2981849062448671d
jeanmmlima <jeanmariomlima@gmail.com>
2024-06-02 12:59:10 -0300

    exemplo com context

--- 16-react-reducer/src/index.js
+++ 16-react-reducer/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Counter from './components/Counter';
-import TodoApp from './components/TodoApp';
+import TodoApp from './components/example-context/TodoApp';
+import { TodoProvider } from './components/example-context/TodoContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoApp />
+    <TodoProvider>
+      <TodoApp />
+    </TodoProvider>
   </React.StrictMode>
 );
 
