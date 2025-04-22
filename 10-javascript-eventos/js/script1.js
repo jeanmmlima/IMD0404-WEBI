8381a9d1ba552b76cc3b53d74a713260133b7e68
jeanmmlima <jeanmariomlima@gmail.com>
2025-04-22 20:16:48 -0300

    novos exemplos

--- 10-javascript-eventos/js/script1.js
+++ 10-javascript-eventos/js/script1.js
@@ -1,25 +1,25 @@
-const contadorElemento = document.getElementById('contador');
-const incrementarBotao = document.getElementById('incrementar');
-const resetarBotao = document.getElementById('resetar');
-const decrementarBotao = document.getElementById('decrementar');
+const contadorElemento = document.getElementById("contador");
+const incrementarBotao = document.getElementById("incrementar");
+const resetarBotao = document.getElementById("resetar");
+const decrementarBotao = document.getElementById("decrementar");
 
 let contador = 0;
 
-  function incrementarContador() {
-    contador++;
-    contadorElemento.textContent = contador;
-  }
+function incrementarContador() {
+  contador++;
+  contadorElemento.textContent = contador;
+}
 
-  function resetarContador() {
-    contador = 0;
-    contadorElemento.textContent = contador;
-  }
+function resetarContador() {
+  contador = 0;
+  contadorElemento.textContent = contador;
+}
 
-  function decrementarContador(){
-    contador--;
-    contadorElemento.textContent = contador;
-  }
+function decrementarContador() {
+  contador--;
+  contadorElemento.textContent = contador;
+}
 
-  incrementarBotao.addEventListener('click', incrementarContador);
-  resetarBotao.addEventListener('click', resetarContador);
-  decrementarBotao.addEventListener('click',decrementarContador);
+incrementarBotao.addEventListener("click", incrementarContador);
+resetarBotao.addEventListener("click", resetarContador);
+decrementarBotao.addEventListener("click", decrementarContador);
